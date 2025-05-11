603b45e0f9df0d2030b0092ee8ba556b8c2773ea
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2025-05-11 12:40:19 +0200

    feat: create ts types

---
+++ types/movie.ts
@@ -0,0 +1,36 @@
+export interface MovieSearchResult {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Year: string;
+}
+
+export interface Movie {
+  imdbID: string;
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: {
+    Source: string;
+    Value: string;
+  }[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  Type: string;
+  DVD?: string;
+  BoxOffice?: string;
+  Production?: string;
+  Website?: string;
+}
