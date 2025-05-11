19e2d96e0499a06d1f39010f359633373a87fd05
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2025-05-11 15:19:12 +0200

    feat: add search functionality sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
@@ -5,20 +7,51 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
+  SidebarInput,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
-import { Home, Star } from "lucide-react";
+import { Label } from "@radix-ui/react-label";
+import { Home, Star, Search } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
 
 export default function AppSidebar() {
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      router.push(`/?q=${encodeURIComponent(searchQuery)}`);
+    }
+  };
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarHeader>
           <h1 className="text-xl font-bold">CineFlix</h1>
+          <form onSubmit={handleSearch}>
+            <SidebarGroup className="py-0">
+              <SidebarGroupContent className="relative">
+                <Label htmlFor="search" className="sr-only">
+                  Search Movies
+                </Label>
+                <SidebarInput
+                  id="search"
+                  placeholder="Search movies..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className="pl-8"
+                />
+                <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </form>
         </SidebarHeader>
         <SidebarGroup>
           <SidebarGroupLabel>Menu</SidebarGroupLabel>
